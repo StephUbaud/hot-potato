b3556cf3f248de711a1973cea82ddaabee5dac84
StephUbaud <92312963+StephUbaud@users.noreply.github.com>
2021-10-18 15:16:42 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    temps = control.millis()
+})
+input.onGesture(Gesture.Shake, function () {
 	
+})
+let temps = 0
+radio.setGroup(1)
+let num = 1
+temps = 0
+basic.forever(function () {
+    if (true) {
+    	
+    }
 })
